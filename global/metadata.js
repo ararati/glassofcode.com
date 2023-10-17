b4314123a23c7303b7bd8d3e2a61858681685f25
Ararat Poghosyan <ararati.poghosyan@gmail.com>
2023-10-16 21:52:14 -0700

    Update site description text

--- global/metadata.js
+++ global/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Glass of Code",
 	url: "https://glassofcode.com/",
 	language: "en",
-	description: "Explore the world of programming, backend systems, and personal IT insights on our blog. Stay updated with the latest trends, tips, and tutorials to enhance your coding skills. Join us on a journey through the digital landscape of technology and discover how to excel in the world of IT.",
+	description: "Programming, Back-End Systems, and a Bit of Gaming and Front-End Apps",
 	author: {
 		name: "Ararat Poghosyan",
 		email: {
